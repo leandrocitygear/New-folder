658fe53772e035841b93b51285628ad6cc5d8b17
Leandro De la cruz <leandrocitygear@icloud.com>
2026-05-07 13:52:45 -0400

    Game working

---
+++ Guess_Num_Game/guess_number_game.py
@@ -0,0 +1,62 @@
+from datetime import date
+import random
+date = date.today()
+formatted_date = date.strftime("%B %d, %Y")
+
+count = 0
+
+def counter():
+    global count
+    count += 1
+    return count
+
+list = []
+
+for i in range(100000):
+   list.append(counter())
+
+
+player_id = random.choice(list)
+
+name = str(input('Enter your name: '))
+age = int(input('Enter your Age: '))
+location = str(input('Where do you live: '))
+welcome = 'Welcome to Guess the number ' + name 
+
+if age < 18:
+    print('too young')
+
+print(f'''
+Hello: {name} 
+Age: {age} 
+Location: {location} 
+player ID: {player_id}
+Account created on: {formatted_date}
+{welcome}
+''')
+
+def start_game():
+
+    play = 'Lets play guess the number to start guess a number between 1 and 10: ' + name
+    print(play)
+    secret = random.randint(1, 10)
+    while True:
+        guess = int(input('enter a number: '))
+        if guess == secret:
+            print('correct')
+            print('You win: ' + name)
+            break
+        else:
+            print('Incorrect try again')
+
+    play_again()
+
+
+def play_again():
+    again = input('would you like to play again: ')
+    if again == 'yes' or again == 'YES':
+        return start_game()
+    else:
+        print('game over')
+
+start_game()
