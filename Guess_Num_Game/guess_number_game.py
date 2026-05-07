545648c1deb33aacfaf98f8bc0c399693620f27a
Leandro De la cruz <leandrocitygear@icloud.com>
2026-05-07 14:33:46 -0400

    making some improvements

--- Guess_Num_Game/guess_number_game.py
+++ Guess_Num_Game/guess_number_game.py
@@ -3,17 +3,14 @@ import random
 date = date.today()
 formatted_date = date.strftime("%B %d, %Y")
 
-count = 0
+# count = 0
 
-def counter():
-    global count
-    count += 1
-    return count
+# def counter():
+#     global count
+#     count += 1
+#     return count
 
-player_ids = []
-
-for i in range(100000):
-   list.append(counter())
+player_ids = list(range(1, 100001))
 
 
 player_id = random.choice(player_ids)
@@ -21,7 +18,7 @@ player_id = random.choice(player_ids)
 name = str(input('Enter your name: '))
 age = int(input('Enter your Age: '))
 location = str(input('Where do you live: '))
-welcome = 'Welcome to Guess the number ' + name 
+welcome = 'Welcome to Guess the number Game ' + name 
 
 if age < 18:
     print('too young')
@@ -41,22 +38,29 @@ def start_game():
     print(play)
     secret = random.randint(1, 10)
     while True:
-        guess = int(input('enter a number: '))
-        if guess == secret:
-            print('correct')
-            print('You win: ' + name)
-            break
-        else:
-            print('Incorrect try again')
-
+        try:
+            guess = int(input('enter a number: '))
+            if guess == secret:
+                print('correct')
+                print('You win: ' + name)
+                break
+            else:
+                print('Incorrect try again')
+        except ValueError:
+            print('Please enter a number')
     play_again()
 
 
 def play_again():
-    again = input('would you like to play again: ')
-    if again == 'yes' or again == 'YES':
-        return start_game()
-    else:
-        print('game over')
+    print('Type [1 for yes] or [0 for no]')
+    again = input('would you like to play again (y/n): ').lower()
+    try:
+        
+        if again == 'y':
+            return start_game()
+        else:
+            print('game over')
 
+    except ValueError:
+        print('please enter (y/n): ')
 start_game()
