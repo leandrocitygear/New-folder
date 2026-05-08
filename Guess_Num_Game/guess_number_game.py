4ab9f7c7c4c6d60b7bc72a10c2dfbba93f07d222
Leandro De la cruz <leandrocitygear@icloud.com>
2026-05-08 14:29:04 -0400

    working on levels

--- Guess_Num_Game/guess_number_game.py
+++ Guess_Num_Game/guess_number_game.py
@@ -4,36 +4,25 @@ import soundfile
 import sounddevice
 import play_song
 
-date = date.today()
-formatted_date = date.strftime("%B %d, %Y")
 
 
-player_ids = list(range(1, 100001))
 
-player_id = random.choice(player_ids)
-
-play_song.play_game_song()
+# play_song.play_game_song()
 name = str(input('Enter your name: '))
-age = int(input('Enter your Age: '))
-location = str(input('Where do you live: '))
-welcome = 'Welcome to Guess the number Game ' + name 
 
 
-print(f'''
-Hello: {name} 
-Age: {age} 
-Location: {location} 
-player ID: {player_id}
-Account created on: {formatted_date}
-{welcome}
-''')
 
 def start_game():
 
     play = 'Lets play guess the number to start guess a number between 1 and 10: ' + name
+    rules = 'Guess the number you get 3 attempts good luck'
+    level1 = 'level 1 pick betweeen 1 and 3'
     print(play)
-    secret = random.randint(1, 10)
+    print(rules)
+    print(level1)
+    secret = random.randint(1, 4)
     auto_win = int(1234)
+    limited_attempts = int(0)
     while True:
         try:
             guess = int(input('enter a number: '))
@@ -45,14 +34,18 @@ def start_game():
                 print('You win: ' + name)
                 quit()
             else:
-                print('Incorrect try again')
+                limited_attempts += 1
+                print(f'Incorrect try again you have: {3 - limited_attempts} left')
+                if limited_attempts == 3:
+                    print('game over')
+                    break
         except ValueError:
             print('Please enter a number')
     play_again()
 
 
 def play_again():
-    again = input('would you like to play again ? or would you like to play something else ? (y/n/m): ').lower()
+    again = input('would you like to play again ? (y/n): ').lower()
 
     try:
         
